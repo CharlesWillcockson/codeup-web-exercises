a08ea2db363d0ff16a9c3ed7f027b4b0173da8a2
Charles Willcockson <willcockson.charles@gmail.com>
2021-11-22 15:16:44 -0600

    exercise 6

--- workingwithoperators.js
+++ workingwithoperators.js
@@ -17,5 +17,31 @@ console.log(perplexed + 2);
 var price = 2.7;
 console.log(price.toFixed(2));
 
-var price = "2.7";
+var price = 2.7;
 console.log(price.toFixed(2));
+
+console.log(isNaN(0))
+
+console.log(isNaN(1))
+
+console.log(isNaN(""))
+
+console.log(isNaN("string"))
+
+console.log(isNaN("0"))
+
+console.log(isNaN("1"))
+
+console.log(isNaN("3.145"))
+
+console.log(isNaN(Number.MAX_VALUE))
+
+console.log(isNaN(Infinity))
+
+console.log(isNaN("true"))
+
+console.log(isNaN(true))
+
+console.log(isNaN("false"))
+
+console.log(isNaN(false))
