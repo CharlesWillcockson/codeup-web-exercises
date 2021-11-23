eb1435eb8e25c2da1fa9816db269e13521a65c9f
Charles Willcockson <willcockson.charles@gmail.com>
2021-11-22 23:21:06 -0600

    adds 3rd exercise from working with operators

---
+++ Workingwithoperators3.js
@@ -0,0 +1,20 @@
+var dayrental = 3.00;
+var lmDays = 3;
+var broBearDays = 5;
+var hercDays = 1;
+var total = ((lmDays + broBearDays + hercDays) * dayrental)
+
+var fbhours = 10;
+var amhours = 4;
+var googhours = 8;
+var Facebook = (350 * fbhours);
+var Amazon = (380 * amhours);
+var Google = (400 * googhours);
+var totalpay = (Facebook + Amazon + Google)
+
+var isfull = true;
+var conflict = true;
+
+var ispremium = true;
+var isexpired = true;
+var numberOfItems = 2;
