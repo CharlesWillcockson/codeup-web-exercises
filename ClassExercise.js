0867e896b0ff2ee35a040f7ef6954e1b0cce18a3
Charles Willcockson <willcockson.charles@gmail.com>
2021-11-23 12:21:01 -0600

    adds exercises from class

---
+++ ClassExercise.js
@@ -0,0 +1,40 @@
+
+
+/*function goodDay (isCloudy, numPlanes) {
+    if ((!isCloudy) && (numPlanes < 10)) {
+        console.log("It is a Missy Elliot day!  Supa Dupa Fly!!")
+    }else {
+        console.log("Just go ahead and drive!")
+    }
+}
+
+console.log(goodDay(false, 4))*/
+
+function add(num1, num2) {
+    return(num1 + num2);
+}
+
+function subtract(num1, num2) {
+    return(num1-num2);
+}
+
+function multiply(num1, num2) {
+    return(num1 *num2);
+}
+
+function divide(num1, num2) {
+    return(num1 / num2);
+}
+
+function remainder(num1, num2) {
+    return(num1 % num2);
+}
+
+console.log(add(12,8));
+console.log(subtract(12,2));
+console.log(multiply(5,2));
+console.log(divide(20,5));
+console.log(remainder(5,2));
+
+
+
