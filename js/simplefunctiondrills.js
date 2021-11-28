7dcfb0f3c218cd01a038c8486827c8f5ed44a90c
Charles Willcockson <willcockson.charles@gmail.com>
2021-11-27 19:25:36 -0600

    adds simple function drills from functions worksheet on github

---
+++ js/simplefunctiondrills.js
@@ -0,0 +1,35 @@
+function returnTwo(){
+    return (2);
+}
+
+console.log(returnTwo(36))
+//--------------------------------
+
+function sayHowdy(){
+    console.log("Howdy!")
+}
+
+sayHowdy()
+//--------------------------------
+
+var yourName = "Charles Willcockson"
+
+function returnName(yourName){
+    return yourName;
+}
+
+console.log(returnName(yourName));
+//-------------------------------------
+
+function addThree(num1){
+    return(num1 + 3);
+}
+
+console.log(addThree(5));
+//-----------------------------------------
+
+function sayString(input){
+    return input;
+}
+
+console.log(sayString('codeup'));
