e3c309dfaf687cfca1ceeeec242695b8d01a1559
Charles Willcockson <willcockson.charles@gmail.com>
2021-12-02 11:46:14 -0600

    adds loop exercises we did in class thurs 12-2

---
+++ js/loop-class-exercises.js
@@ -0,0 +1,19 @@
+// let i = 0;
+//
+// while (i < 10){
+//     console.log("iteration: ", i);
+//     i++;
+// }
+
+let emptySpotz = 0;
+
+while (emptySpotz <= 20){
+    console.log("There are " + emptySpotz + " parking spots available");
+    emptySpotz++;
+}
+
+let i = 0;
+while (i <= 15){
+    i++;
+    console.log(i)
+}
