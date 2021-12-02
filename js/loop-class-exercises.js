eef50817c335a0dbff7c4e180df511add1867eda
Charles Willcockson <willcockson.charles@gmail.com>
2021-12-02 11:59:34 -0600

    adds do while loop exercises

--- js/loop-class-exercises.js
+++ js/loop-class-exercises.js
@@ -5,15 +5,28 @@
 //     i++;
 // }
 
-let emptySpotz = 0;
-
-while (emptySpotz <= 20){
-    console.log("There are " + emptySpotz + " parking spots available");
-    emptySpotz++;
-}
-
+// let emptySpotz = 0;
+//
+// while (emptySpotz <= 20){
+//     console.log("There are " + emptySpotz + " parking spots available");
+//     emptySpotz++;
+// }
+//
+// let i = 0;
+// while (i <= 15){
+//     i++;
+//     console.log(i)
+// }
+//Do While----------------------------------------------------
 let i = 0;
-while (i <= 15){
-    i++;
+// do{
+//     i++
+//     console.log(i);
+// }while (i <= 15);
+
+
+
+do {
     console.log(i)
-}
+    i = (i + 2);
+}while (i <= 50);
