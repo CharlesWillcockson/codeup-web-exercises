a74d45024c822344d419801f185551e5a5e085d3
Charles Willcockson <willcockson.charles@gmail.com>
2021-12-03 10:00:47 -0600

    finished while loops

--- js/while.js
+++ js/while.js
@@ -1,18 +1,21 @@
-// let i = 2;
-//
-// while (i <= 70000){
-//     console.log(i);
-//     i = (i * 2);
-// }
+let i = 2;
+
+while (i <= 70000){
+    console.log(i);
+    i = (i * 2);
+}
 
 var allCones = Math.floor(Math.random() * 50) + 50;
 
 do {
     let conesUsed = Math.floor(Math.random() * 5) + 1;
-    if (allCones > conesUsed){
+    if (allCones >= conesUsed){
         console.log("You sold " + conesUsed + " cones!" + " You now have " + allCones + " cones left");
-    }else if(allCones < conesUsed){
+        allCones -= conesUsed;
+    }else {
         console.log("You cannot make a sale for " + conesUsed + " cones, you only have " + allCones + " cones left!")
-    }else console.log("You are sold out!")
-    allCones = (allCones - conesUsed);
+    }
+    console.log(allCones)
 } while (allCones > 0);
+
+console.log("Yay, all the cones are sold!");
