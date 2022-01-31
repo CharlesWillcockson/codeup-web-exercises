a14e5b83ffda9f96cac6f017607d0d936c92287a
Charles Willcockson <willcockson.charles@gmail.com>
2022-01-31 00:48:58 -0600

    working through map filter reduce

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,66 @@
+"use strict";
+
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+// use .filter to return an array of people who speak 3 or more languages...
+
+const threeLanguages = users.filter(user => user.languages.length >= 3);
+console.log(threeLanguages);
+
+// Use .map to create an array of strings where each element is a user's email address
+
+const userEmail = users.map(user => user.email);
+console.log(userEmail);
+
+// Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
+
+let totalYears = users.reduce((total, current) => {
+    return (total + current.yearsOfExperience);
+}, 0) / users.length;
+console.log(totalYears);
+
+// Use .reduce to get the longest email from the list of users.
+
+let longestEmail = userEmail.reduce((longest, current) => (current.length > longest.length ? current : longest));
+
+console.log(longestEmail);
+
+// Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
+
+let userString = `Your instructors are: ${users.reduce}`
